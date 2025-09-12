4b7bf1460a6651e9dd32ffce0aeb48104f8c1a4b
ohspc89 <jinseok.oh@pm.me>
2025-09-11 21:24:58 -0700

    Spyder version

--- contents/week1/Week1_assignment.py
+++ contents/week1/Week1_assignment.py
@@ -8,29 +8,31 @@ Created on Sun Aug 24 00:42:28 2025
 
 # %% Task 1. Read 'week1.json' and save its content to a variable: 'data'
 
-# answer:
-# import json
-# with open('/home/jinseok/Downloads/week1.json', 'r', encoding='utf-8') as f:
-#     data = json.load(f)
+import urllib.request   # This is package to read files using URL
+import ...              # You need to import one more library
+json_url = "https://raw.githubusercontent.com/ohspc89/reprorehab2025/students/contents/week1/week1.json"
+# Complete '...' below.
+# Here, instead of `json.load()` method introduced in the discussion,
+# use `json.loads()` method.
+# This is because we're reading text files using url.
+with urllib.request.urlopen(json_url) as f:
+    data = ...(f.read())
+    
     
 # %% Task 2. Report the first-level keys of 'data'. How many subjects?
 # hint: use `len()` function to get the length of a sequence
 
-# answer:
-# len(data.keys())
+len(...)
+
 
 # %% Task 3. Report the second-level keys of 'data'.
 # How many days each participant was tracked?
 # hint: you can use the first key at the first-level
 
-# answer:
-# data['sub0'].keys()
 
 # %% Task 4. What were the measures of each day?
 # hint: all three days were the same regarding measures
 
-# answer:
-# data['sub0']['day1']
 
 # %% Task 5. Make a new dictionary, 'sleep_hours'.
 # Its structure will be like:
@@ -41,34 +43,30 @@ Created on Sun Aug 24 00:42:28 2025
 # Task 5a: First create three lists, each of which will be the value
 # of 'sleep_hours'. Use list comprehensions.
 
-# answer:
-# day1_value = [value['day1']['hour_slept'] for key, value in data.items()]
-# day2_value = [value['day2']['hour_slept'] for key, value in data.items()]
-# day3_value = [value['day3']['hour_slept'] for key, value in data.items()]
+day1_value = ...
+day2_value = ...
+day3_value = ...
 
 # Task 5b: Now make 'sleep_hours'
 
-# answer:
-# sleep_hours = {'day1': day1_value,
-#                'day2': day2_value,
-#                'day3': day3_value}
+sleep_hours = ...
+
 
 # %% Task 6. Calculate the mean and the standard deviation of hours slept
 # on each day using 'sleep_hours' dictionary of Task 5.
 # Make two variables ('means' and 'stds') using list comprehensions.
-# hint: import numpy and use numpy.mean() and numpy.std() function
+# hint: import numpy and use `numpy.mean()` and `numpy.std()`
 
-# answer:
-# import numpy as np
-# means = [np.mean(value) for key, value in sleep_hours.items()]
-# stds = [np.std(value) for key, value in sleep_hours.items()]
+import ...
+means = ...
+stds = ...
+
 
 # %% Task 7. Plot daily sleep hour means using 'means' sequence prepared in Task 6.
 # Make sure that your days start from 1. What should you do then?
 # requirement: use `range()`
 
-# answer:
-# import matplotlib.pyplot as plt
-# plt.plot(range(1, 4), means)
-# plt.xlabel('Days')
-# plt.ylabel('Hours slept')
+import matplotlib.pyplot as plt
+plt.plot(...)       # provide X and Y
+plt.xlabel(...)     # provide x-axis label
+plt.ylabel(...)     # provide y-axis label
